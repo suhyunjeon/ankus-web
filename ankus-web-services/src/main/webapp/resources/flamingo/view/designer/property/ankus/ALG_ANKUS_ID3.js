e6c27f8072506913d35c5c01ce19003f11028f8d
Su Hyun Jeon <suhyunjun@gmail.com>
2014-07-23 16:40:05 +0900

    Update ALG_ANKUS_ID3.js
    Changed ankus-core version to 0.2 from 0.1

--- ankus-web-services/src/main/webapp/resources/flamingo/view/designer/property/ankus/ALG_ANKUS_ID3.js
+++ ankus-web-services/src/main/webapp/resources/flamingo/view/designer/property/ankus/ALG_ANKUS_ID3.js
@@ -603,7 +603,7 @@ Ext.define('Flamingo.view.designer.property.ankus.ALG_ANKUS_ID3', {
                     xtype: 'textfield',
                     name: 'jar',
                     fieldLabel: MSG.DESIGNER_MR_JAR,
-                    value: 'org.ankus:ankus-core:0.1',
+                    value: 'org.ankus:ankus-core:0.2',
                     disabledCls: 'disabled-plain',
                     allowBlank: false
                 },
